5d73e635e2dc50bb8e0f00087d3bd693ce17b2c3
lexuanhai <emcuahai@gmail.com>
2022-06-07 13:17:30 +0700

    update trang chu gioi thieu

---
+++ js/header/header.js
@@ -0,0 +1,16 @@
+(function() {
+
+   window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        $(".box-menu").css("padding","10px 10px 10px 10px");
+         $(".nav-link").css("font-size","14px"); 
+         $(".navbar-branch.logo").css({"height":"50px","width":"136px"});
+      } else {
+         $(".box-menu").css("padding","20px 50px 20px 50px");
+         $(".navbar-branch.logo").css({"height":"60px","width":"162px"}); 
+         $(".nav-link").css("font-size","17px"); 
+      }
+    }
+})();
